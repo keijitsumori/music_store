34502cec5ac7f3d5baa7345df1ed81c0b280bebe
EC2 Default User <ec2-user@ip-172-31-42-201.ec2.internal>
2022-01-29 16:02:21 +0000

    [devise] make_devise_routes

--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,9 @@
 Rails.application.routes.draw do
-  devise_for :admins
-  devise_for :customers
-  # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
+  devise_for :customers, controllers: {
+    registrations: "customer/registrations",
+    sessions: 'customer/sessions'
+  }
+  devise_for :admin, controllers: {
+    sessions: "admin/sessions"
+  }
 end
