2ff3db209d629e343b3ca723cc153511aff1fcb1
EC2 Default User <ec2-user@ip-172-31-42-201.ec2.internal>
2022-01-29 15:34:10 +0000

    [model] make genres

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2022_01_29_153105) do
+ActiveRecord::Schema.define(version: 2022_01_29_153225) do
 
   create_table "addresses", force: :cascade do |t|
     t.integer "customer_id", null: false
@@ -25,6 +25,12 @@ ActiveRecord::Schema.define(version: 2022_01_29_153105) do
     t.integer "item_id", null: false
     t.integer "customer_id", null: false
     t.integer "amount", null: false
+    t.datetime "created_at", precision: 6, null: false
+    t.datetime "updated_at", precision: 6, null: false
+  end
+
+  create_table "genres", force: :cascade do |t|
+    t.string "name", null: false
     t.datetime "created_at", precision: 6, null: false
     t.datetime "updated_at", precision: 6, null: false
   end
