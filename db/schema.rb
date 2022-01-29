a813937b03b34d0162e10e0f63e089f18ac85bd0
EC2 Default User <ec2-user@ip-172-31-42-201.ec2.internal>
2022-01-29 15:36:21 +0000

    [model] make items

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2022_01_29_153225) do
+ActiveRecord::Schema.define(version: 2022_01_29_153503) do
 
   create_table "addresses", force: :cascade do |t|
     t.integer "customer_id", null: false
@@ -31,6 +31,17 @@ ActiveRecord::Schema.define(version: 2022_01_29_153225) do
 
   create_table "genres", force: :cascade do |t|
     t.string "name", null: false
+    t.datetime "created_at", precision: 6, null: false
+    t.datetime "updated_at", precision: 6, null: false
+  end
+
+  create_table "items", force: :cascade do |t|
+    t.integer "genre_id", null: false
+    t.string "name", null: false
+    t.string "image_id", null: false
+    t.text "introduction", null: false
+    t.integer "price", null: false
+    t.boolean "is_active", default: true, null: false
     t.datetime "created_at", precision: 6, null: false
     t.datetime "updated_at", precision: 6, null: false
   end
