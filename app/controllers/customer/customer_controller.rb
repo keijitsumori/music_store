ae1e0daa97b23f5d10b2c72899f8e5747d4d37dd
EC2 Default User <ec2-user@ip-172-31-42-201.ec2.internal>
2022-01-30 02:04:25 +0000

    [customer] make sign_up_system

---
+++ app/controllers/customer/customer_controller.rb
@@ -0,0 +1,19 @@
+class Customer::CustomerController < ApplicationController
+    def show
+    end
+
+    def edit
+    end
+
+    def update
+    end
+
+    def unsubscribe
+    end
+
+    def withdraw
+        current_customer.update(is_deleted: true)
+        reset_session
+        redirect_to root_path
+    end
+end
