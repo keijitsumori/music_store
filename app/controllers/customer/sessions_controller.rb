7cc36f2cc43a7ab346d0d633cdde5f556bf862d1
EC2 Default User <ec2-user@ip-172-31-42-201.ec2.internal>
2022-01-30 05:04:25 +0000

    [devise] make log_in_system

--- app/controllers/customer/sessions_controller.rb
+++ app/controllers/customer/sessions_controller.rb
@@ -1,7 +1,12 @@
 # frozen_string_literal: true
 
 class Customer::SessionsController < Devise::SessionsController
+  before_action :configure_is_deleted?,  only: [:create]
   # before_action :configure_sign_in_params, only: [:create]
+  
+  def after_sign_in_path_for(resource)
+    customers_my_page_path # ログイン後に遷移するpathを設定
+  end
 
   # GET /resource/sign_in
   # def new
@@ -17,6 +22,18 @@ class Customer::SessionsController < Devise::SessionsController
   # def destroy
   #   super
   # end
+  
+  protected
+
+  def configure_is_deleted?
+    @customer = Customer.find_by(email: params[:customer][:email])
+    return if !@customer
+    if @customer
+      if (@customer.valid_password?(params[:customer][:password]) && (@customer.active_for_authentication? == false))
+        redirect_to new_customer_registration_path, notice: "退会済みです。"
+      end
+    end
+  end
 
   # protected
 
