095fe53e42dfb56301b25833aed526f939ef8b79
EC2 Default User <ec2-user@ip-172-31-42-201.ec2.internal>
2022-01-31 07:52:42 +0000

    [model] make validates

--- app/models/order_detail.rb
+++ app/models/order_detail.rb
@@ -1,2 +1,6 @@
 class OrderDetail < ApplicationRecord
+  belongs_to :order
+  belongs_to :item
+
+  enum making_status: { couldnt_make: 0, waiting_for_production: 1, in_production: 2, completed: 3 }
 end
