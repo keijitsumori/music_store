fb610d9fd274456c9186c840ec5e1c60674d8d81
EC2 Default User <ec2-user@ip-172-31-42-201.ec2.internal>
2022-01-31 07:14:20 +0000

    [customer] item show

--- app/controllers/customer/items_controller.rb
+++ app/controllers/customer/items_controller.rb
@@ -4,4 +4,10 @@ class Customer::ItemsController < ApplicationController
         @genres = Genre.all
         @items = @items.where(genre_id: params[:genre_id].to_i) if params[:genre_id].present?
     end
+    
+    def show
+        @genres = Genre.all
+        @item = Item.find(params[:id])
+        @cart_item = CartItem.new
+    end
 end
