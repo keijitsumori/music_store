89e3194dcc78e012ceb4946b53b9ca55a21cf0d3
EC2 Default User <ec2-user@ip-172-31-42-201.ec2.internal>
2022-01-31 12:52:40 +0000

    [order] make order management

---
+++ app/controllers/admin/order_details_controller.rb
@@ -0,0 +1,36 @@
+class Admin::OrderDetailsController < ApplicationController
+    def update
+        order_detail = OrderDetail.find(params[:id])
+        if params[:order_detail][:making_status] == 'in_production'
+            order_detail.update(making_status: 2)
+            order = Order.find_by(id: order_detail.order_id)
+            order.update(status: 2)
+            redirect_to admin_orders_path
+        elsif params[:order_detail][:making_status] == 'completed'
+            order_detail.update(making_status: 3)
+            order_details = OrderDetail.where(order_id: order_detail.order_id)
+            a = 0
+            order_details.each do |order_detail|
+                unless order_detail.making_status != 'completed'
+                    a += 1
+                end
+            end
+            if a == order_details.size
+                order = Order.find_by(id: order_detail.order_id)
+                order.update(status: 3)
+                redirect_to admin_orders_path
+            else
+                redirect_to admin_orders_path
+            end
+        else
+            order_detail.update(making_status: (OrderDetail.making_statuses[params[:order_detail][:making_status].to_sym]).to_i)
+            redirect_to admin_orders_path
+        end
+    end
+
+    private
+
+    def order_detail_params
+        params.require(:order_detail).permit(:making_status)
+    end
+end
---
+++ app/controllers/admin/orders_controller.rb
@@ -0,0 +1,39 @@
+class Admin::OrdersController < ApplicationController
+    before_action :check_admin, only: [:index, :show]
+
+    def index
+        @orders = Order.all
+    end
+
+    def show
+        @order = Order.find(params[:id])
+        @order_details = OrderDetail.where(order_id: @order.id)
+    end
+
+    def update
+        @order = Order.find(params[:id])
+        if params[:order][:status] == 'comfirm'
+            @order.update(status: (Order.statuses[params[:order][:status].to_sym]).to_i)
+            order_details = OrderDetail.where(order_id: @order.id)
+            order_details.each do |order_detail|
+                order_detail.update(making_status: 1)
+            end
+            redirect_to admin_orders_path
+        else
+            @order.update(status: params[:order][:status].to_i)
+            redirect_to admin_orders_path
+        end
+    end
+
+    private
+
+    def check_admin
+        unless admin_signed_in?
+            redirect_to root_path
+        end
+    end
+
+    def order_params
+        params.require(:order).permit(:id, :status)
+    end
+end
